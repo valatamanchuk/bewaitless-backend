46ead993859dda82242ceb7c2da75b7dccaed4b4
Valerii Atamanchuk <frolapps@gmail.com>
2018-06-21 17:35:58 +0300

    procfile

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,5 +2,7 @@ import Vapor
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    
+    router.get { req in
+        return Environment.get("testkey") ?? "none"
+    }
 }
